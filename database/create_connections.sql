bd1d619bba7cbc2c807db9e379be8a5c9161320b
ddegagne <deric.degagne@outlook.com>
2015-08-16 19:12:21 -0400

    Update create_connections.sql

--- database/create_connections.sql
+++ database/create_connections.sql
@@ -6,10 +6,9 @@ use vpn_connections;
 
 CREATE TABLE connections
   (
-    id            int(11) NOT NULL AUTO_INCREMENT,
-    name          varchar(45) DEFAULT NULL,
-    country_code  varchar(2) DEFAULT NULL,
-    file_path     varchar(150) DEFAULT NULL,
-    current       varchar(1) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    id            INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(45) DEFAULT NULL,
+    country_code  VARCHAR(2) DEFAULT NULL,
+    file_path     VARCHAR(150) DEFAULT NULL,
+    current       VARCHAR(1) DEFAULT NULL
   )

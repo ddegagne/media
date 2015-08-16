388ce563ad02eaf73b79d6dddb5e1190bb883231
ddegagne <deric.degagne@outlook.com>
2015-08-16 13:15:28 -0400

    Create create_auth.sql

---
+++ database/create_auth.sql
@@ -0,0 +1,15 @@
+
+-- Create auth table
+--
+
+use connections;
+
+CREATE TABLE auth
+  (
+    id       INT(6) UNSIGNED auto_increment PRIMARY KEY,
+    service  VARCHAR(100) NOT NULL,
+    port     VARCHAR(6),
+    url      VARCHAR(100) NOT NULL,
+    username VARCHAR(20),
+    password VARCHAR(45)
+  )

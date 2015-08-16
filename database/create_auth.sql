d19ccd0480cf0fb052898c67c9335f285efa6188
ddegagne <deric.degagne@outlook.com>
2015-08-16 19:49:38 -0400

    Update create_auth.sql

--- database/create_auth.sql
+++ database/create_auth.sql
@@ -1,15 +1,19 @@
 --
--- Create auth table
+-- Table structure for table `auth`
 --
 
 use vpn_connections;
 
-CREATE TABLE auth
-  (
-    id       INT(6) UNSIGNED auto_increment PRIMARY KEY,
-    service  VARCHAR(100) NOT NULL,
-    port     VARCHAR(6),
-    url      VARCHAR(100) NOT NULL,
-    username VARCHAR(20),
-    password VARCHAR(45)
-  )
+DROP TABLE IF EXISTS `auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `service` varchar(100) NOT NULL,
+  `port` varchar(6) DEFAULT NULL,
+  `url` varchar(100) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
